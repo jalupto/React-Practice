26b87c0e8c3978df025481925303e387923060cf
Jared Upton <jalupto@gmail.com>
2021-06-22 17:23:57 -0400

    finish React modules 1-4

--- src/components/site/Sidebar.js
+++ src/components/site/Sidebar.js
@@ -5,6 +5,9 @@ import {
 } from 'react-router-dom';
 import Home from './Home';
 import Resources from './Resources';
+import FunctionalComponentDemo from '../concepts/FunctionalComponentDemo';
+import JSXRules from '../concepts/JSXRules';
+import State from '../concepts/State';
 
 const Sidebar = () => {
     return(
@@ -14,6 +17,8 @@ const Sidebar = () => {
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/resources'>Resources</Link></li>
                     <li><Link to='/functionalcomponent'>Functional Component</Link></li>
+                    <li><Link to='/jsxrules'>JSX Rules</Link></li>
+                    <li><Link to='/state'>State</Link></li>
                 </ul>
             </div>
             <div className='sidebar-route'>
@@ -21,6 +26,9 @@ const Sidebar = () => {
                     <Route exact path='/home'><Home /></Route>
                     <Route exact path='/resources'><Resources /></Route>
                     <Route exact path='/'><Home /></Route>
+                    <Route exact path='/functionalcomponent'><FunctionalComponentDemo /></Route>
+                    <Route exact path='/jsxrules'><JSXRules /></Route>
+                    <Route exact path='/state'><State /></Route>
                 </Switch>
             </div>
         </div>
