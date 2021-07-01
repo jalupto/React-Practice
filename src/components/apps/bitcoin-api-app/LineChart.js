5b63857a23c979172ed004e73cf72869b067f7e1
Jared Upton <jalupto@gmail.com>
2021-07-01 19:49:48 -0400

    trying to fix chart.js issue

--- src/components/apps/bitcoin-api-app/LineChart.js
+++ src/components/apps/bitcoin-api-app/LineChart.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Chart from 'chart.js';
+import Chart from 'chart.js/auto';
 import moment from 'moment';
 
 const LineChart = (props) => {
